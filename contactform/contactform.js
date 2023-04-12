92bb9837c234ae282bf53f4cacbc1b7471032258
PichardoWilfred <pichardowilfred@gmail.com>
2023-04-12 15:30:49 -0400

    adjusting some changes and validating the form fully

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -3,14 +3,13 @@ jQuery(document).ready(function($) {
 
   //Contact
   $('form.contact-form').submit(function(e) {
-    var f = $(this).find('.form-group'),
+    var form = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-    f.children('input').each(function() { // run all inputs
-
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
+    form.children('input').each(function() { // run all inputs
+      var input = $(this); // current input
+      var rule = input.attr('data-rule');
 
       if (rule !== undefined) {
         var ierror = false; // error flag for current input
@@ -24,41 +23,42 @@ jQuery(document).ready(function($) {
 
         switch (rule) {
           case 'required':
-            if (i.val() === '') {
+            if (input.val() === '') {
               ferror = ierror = true;
             }
             break;
 
           case 'minlen':
-            if (i.val().length < parseInt(exp)) {
+            if (input.val().length < parseInt(exp)) {
               ferror = ierror = true;
             }
             break;
 
           case 'email':
-            if (!emailExp.test(i.val())) {
+            if (!emailExp.test(input.val())) {
               ferror = ierror = true;
             }
             break;
 
           case 'checked':
-            if (!i.attr('checked')) {
+            if (!input.attr('checked')) {
               ferror = ierror = true;
             }
             break;
 
           case 'regexp':
             exp = new RegExp(exp);
-            if (!exp.test(i.val())) {
+            if (!exp.test(input.val())) {
               ferror = ierror = true;
             }
             break;
         }
-        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+        // const error_label = input.next('.validation')[0];
+        // console.log(error_label);
+        input.next('.validation').html((ierror ? (input.attr('data-msg') !== undefined ? input.attr('data-msg') : 'wrong Input') : '')).show();
       }
     });
-    f.children('textarea').each(function() { // run all inputs
-
+    form.children('textarea').each(function() { // run all inputs
       var i = $(this); // current input
       var rule = i.attr('data-rule');
 
@@ -85,10 +85,31 @@ jQuery(document).ready(function($) {
             }
             break;
         }
-        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+        // const error_label = i.next('.validation')[0];
+        // console.log(error_label);
+        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show();
       }
     });
-    // if (ferror) return false;
+    if (!ferror) { // if it has no errors then...
+      const serviceID = 'service_b16wdlt';
+      const templateID = 'template_quxrm0j';
+      
+      $("div.form").addClass("loading");
+      // return false;
+      emailjs.sendForm(serviceID, templateID, this).then(() => {
+        $("#sendmessage").addClass("show");
+        $("div.form").removeClass("loading");
+        $("#errormessage").removeClass("show");
+        $('.contactForm').find("input, textarea").val("");
+        $('button#submit-btn').prop('disabled', true);
+      }, (err) => {
+        console.error(err);
+        $("#sendmessage").removeClass("show");
+        $("#errormessage").addClass("show");
+        $('#errormessage').html(err);
+      });
+    }
+    return false;
     // else var str = $(this).serialize();
     // $.ajax({
     //   type: "POST",
@@ -97,26 +118,13 @@ jQuery(document).ready(function($) {
     //   success: function(msg) {
     //     // alert(msg);
     //     if (msg == 'OK') {
-    //       $("#sendmessage").addClass("show");
-    //       $("#errormessage").removeClass("show");
-    //       $('.contactForm').find("input, textarea").val("");
+
     //     } else {
-    //       $("#sendmessage").removeClass("show");
-    //       $("#errormessage").addClass("show");
-    //       $('#errormessage').html(msg);
+
     //     }
     //   }
     // });
     
-    const serviceID = 'service_b16wdlt';
-    const templateID = 'template_quxrm0j';
-
-    emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-    }, (err) => {
-    console.error(err)
-    });
-    return false;
   });
 
 });
