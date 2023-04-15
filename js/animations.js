28b8ba7ec4998d28cd63efcc4db5a1bc7b89b2b8
PichardoWilfred <pichardowilfred@gmail.com>
2023-04-15 11:44:41 -0400

    logotype desktop floating animation

--- js/animations.js
+++ js/animations.js
@@ -1,13 +1,25 @@
 const fragments = $('#fragments')[0];
 const base_logo = $('#base-logo')[0];
 
+const title_container = $('#title-container')[0]; 
+
 export function animate_portrait() {
     base_logo.classList.add('slide-in');
     fragments.classList.add('slide-in');
 }
 fragments.addEventListener('animationend', ({target}) => {
     if (target.classList.contains('slide-in')) {
+
         fragments.classList.add('float');
+        base_logo.classList.add('float');
+        
         fragments.classList.remove('slide-in');
+        base_logo.classList.remove('slide-in');
     }
-})
+});
+
+fragments.addEventListener('animationend', ({target}) => {
+    if ( target.classList.contains('float') ) {
+        title_container.classList.add('showing');
+    }
+});
