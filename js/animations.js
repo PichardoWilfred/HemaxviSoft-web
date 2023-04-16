86d25fb74eb37b94e943e972bf4cc5f2aaa90050
PichardoWilfred <pichardowilfred@gmail.com>
2023-04-16 18:41:28 -0400

    portrait finished

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,11 @@
 const fragments = $('#fragments')[0];
 const base_logo = $('#base-logo')[0];
-
 const title_container = $('#title-container')[0]; 
 
 export function animate_portrait() {
+    const viewport_lg = window.matchMedia("(min-width: 992px)").matches;
+    $('#company-description').removeClass('invisible')
+    $('#company-description').addClass(viewport_lg ? 'fadeInLeft': 'fadeInUp');
     base_logo.classList.add('slide-in');
     fragments.classList.add('slide-in');
 }
